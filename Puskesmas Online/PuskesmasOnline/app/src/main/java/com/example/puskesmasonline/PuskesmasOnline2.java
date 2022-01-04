84678c45fbc533ca0e7f3f7995dcfcd6c4b614fd
fahruladitya <69379966+fahruladitya@users.noreply.github.com>
2022-01-04 21:16:50 +0700

    Add files via upload

---
+++ Puskesmas Online/PuskesmasOnline/app/src/main/java/com/example/puskesmasonline/PuskesmasOnline2.java
@@ -0,0 +1,14 @@
+package com.example.puskesmasonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PuskesmasOnline2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puskesmas_online2);
+    }
+}
